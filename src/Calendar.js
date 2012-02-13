ae634a9e657d23b9c4c2d8eba104d5e0466eba5a
Thomas B <roundcube@gmail.com>
2012-02-13 19:12:13 +0100

    New option to define the max. height of a month view day cell and to hide overflowing events

--- src/Calendar.js
+++ src/Calendar.js
@@ -514,6 +514,8 @@ function Calendar(element, options, eventSources) {
 		if (name == 'height' || name == 'contentHeight' || name == 'aspectRatio') {
 			options[name] = value;
 			updateSize();
+		} else if (name == 'maxHeight') {
+			options[name] = value;
 		}
 	}
 	
